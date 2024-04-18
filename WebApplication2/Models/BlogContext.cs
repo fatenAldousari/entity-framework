45439f11c94e9b6b0f87c178487e6fa4e271cf72
KFH <KFH@LAPTOP-TRN-140>
2024-04-18 15:27:45 +0300

    Add project files.

---
+++ WebApplication2/Models/BlogContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().HasData(
+                new Blog
+                {
+                    Id = 1,
+                    Name = "Food Blog",
+                    Url = "https://www.myfood.eat"
+                }
+            );
+        }
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            optionsBuilder.UseSqlite("Data Source=asp.dp");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
